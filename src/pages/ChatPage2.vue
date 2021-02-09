2aced9c320625a85af9de190b586a8bd4eb1b5d4
tuanama13 <tuanama7@gmail.com>
2021-02-09 17:31:44 +0700

    Update UI

--- src/pages/ChatPage2.vue
+++ src/pages/ChatPage2.vue
@@ -4,48 +4,56 @@
       <q-scroll-area
         @scroll="scrollPosition"
         ref="scroll"
-        style="height: 79vh;"
+        style="height: 75vh;"
+        class="q-px-md"
       >
-        <q-chat-message label="Sunday, 19th" />
-
+        <!-- <q-chat-message label="Sunday, 19th" /> -->
         <div v-for="(item, index) in allMessages" :key="index">
           <q-chat-message
             v-if="item.userId == 2"
-            :name="item.user.name"
             avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-            :text="[item.message]"
             sent
             stamp="7 minutes ago"
-          />
+          >
+            {{ item.message }}
+            <img v-if="item.image" :src="item.image" height="100px" />
+          </q-chat-message>
           <q-chat-message
             v-else
-            :name="item.user.name"
             avatar="https://cdn.quasar.dev/img/avatar4.jpg"
-            :text="[item.message]"
             stamp="4 minutes ago"
-          />
+          >
+            {{ item.message }}
+            <img v-if="item.image" :src="item.image" height="100px" />
+          </q-chat-message>
         </div>
       </q-scroll-area>
-      <q-input
-        @keyup.enter="sendMessage"
-        outlined
-        bottom-slots
-        v-model="message"
-        :dense="dense"
-      >
-        <template v-slot:append>
-          <q-icon
-            v-if="message !== ''"
-            name="close"
-            @click="message = ''"
-            class="cursor-pointer"
-          />
-        </template>
-
-        <template v-slot:after>
-          <q-btn @click="sendMessage" round :dense="dense" flat icon="send" />
-        </template>
-      </q-input>
+      <div class="q-py-md">
+        <q-input
+          @keyup.enter="sendMessage"
+          outlined
+          bottom-slots
+          v-model="message"
+        >
+          <template v-slot:append>
+            <q-btn
+              @click="$refs.FileInput.pickFiles()"
+              round
+              :dense="dense"
+              flat
+              icon="attach_file"
+            />
+            <q-btn @click="sendMessage" round :dense="dense" flat icon="send" />
+          </template>
+        </q-input>
+      </div>
+      <q-file
+        ref="FileInput"
+        style="display:none"
+        v-model="file"
+        @input="uploadImage"
+        accept=".jpg, image/*"
+      />
     </div>
   </div>
 </template>
@@ -57,16 +65,11 @@ export default {
     return {
       allMessages: [],
       message: "",
-      // user: {
-      //   id: 1,
-      //   name: "Joko"
-      // },
       user: {
         id: 2,
         name: "Jono"
       },
-      ph: "",
-
+      file: null,
       dense: false
     };
   },
@@ -75,20 +78,22 @@ export default {
   },
   methods: {
     async fetchMessage() {
-      await this.$db
-        .collection("chats")
-        .orderBy("createdAt")
-        .onSnapshot(result => {
-          let allMessages = [];
-          result.forEach(doc => {
-            // doc.data() is never undefined for query doc snapshots
-            allMessages.push(doc.data());
-            // console.log(doc.id, " => ", doc.data());
+      if (this.allMessages.legth != 0) {
+        await this.$db
+          .collection("chats")
+          .orderBy("createdAt")
+          .onSnapshot(result => {
+            let allMessages = [];
+            result.forEach(doc => {
+              // doc.data() is never undefined for query doc snapshots
+              allMessages.push(doc.data());
+              // console.log(doc.id, " => ", doc.data());
+            });
+            this.allMessages = allMessages;
+            this.$refs.scroll.setScrollPosition(909, 1);
           });
-          this.allMessages = allMessages;
-          this.$refs.scroll.setScrollPosition(909, 1);
-        });
-      this.$refs.scroll.setScrollPercentage(1);
+        this.$refs.scroll.setScrollPercentage(1);
+      }
     },
     sendMessage() {
       // Add a new document in collection "cities"
@@ -96,7 +101,29 @@ export default {
         userId: this.user.id,
         user: this.user,
         message: this.message,
+        image: this.file
+          ? "https://firebasestorage.googleapis.com/v0/b/vue-chat-4681a.appspot.com/o/" +
+            image +
+            "?alt=media"
+          : null,
         createdAt: new Date()
+      });
+      this.message = null;
+      this.file = null;
+      this.fetchMessage();
+    },
+    uploadImage(file) {
+      // Add a new document in collection "cities"
+      const ref = this.$firebase
+        .storage()
+        .ref()
+        .child(file.name);
+
+      // [START storage_upload_blob]
+      // 'file' comes from the Blob or File API
+      ref.put(file).then(snapshot => {
+        console.log("Uploaded a blob or file!");
+        this.sendMessage(file.name);
       });
       this.message = null;
       this.fetchMessage();
