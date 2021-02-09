e16ddaeb81b166ec6e8a53ecca746959a7411be2
tuanama13 <tuanama7@gmail.com>
2021-02-09 14:58:37 +0700

    Update Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,8 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Vue + Firebase Realtime Chat
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -26,9 +24,7 @@
       content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
-        </q-item-label>
+        <q-item-label header class="text-grey-8"> </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
           :key="link.title"
